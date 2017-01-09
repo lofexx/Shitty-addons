6bc1d5013f90b98379a9ad165a579c03dbb0dab8
lofexx <lofbom12@gmail.com>
2017-01-09 01:06:24 +0100

    hi
    hi

--- Xerath/Xerath/Program.cs
+++ Xerath/Xerath/Program.cs
@@ -3,7 +3,7 @@ using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Menu.Values;
-    //why are u checking out my spaghetti Zaloopa?
+    
 
 namespace Xerath
 {
@@ -12,24 +12,25 @@ namespace Xerath
         static void Main(string[] args)
         {
             Loading.OnLoadingComplete += OnLoadingComplete;
-       //chaos teach me some of ur coding skillz
+      
 
 
         }
 
         private static void OnLoadingComplete(EventArgs args)
-        {
+        {   
             if (Player.Instance.Hero != Champion.Xerath) return;
+            Spells.InitializeSpells();
+            Menus.CreateMenu();
 
             Chat.Print("<font color = #000099>Xerath Loaded</font>");
             //this prints my shit on the chat
             Game.OnTick += OnTick;
             //uses ontick
 
-            Spells.InitializeSpells();
-            Menus.CreateMenu();
+            
 
-            //fuck my life
+            
         }
 
         private static void OnTick(EventArgs args)
@@ -49,3 +50,7 @@ namespace Xerath
 
     //report melanie for doxing me
     //Wanna swap name yorik100?
+    //nice reputation eggbrother
+    //fuck my life got doxed by melanie kappa123
+ //chaos teach me some of ur coding skillz
+//why are u checking out my spaghetti Zaloopa?
